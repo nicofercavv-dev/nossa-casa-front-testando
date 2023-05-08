411b4d2e020dbc25baea03d6c65a66c0edb20d07
renatu5 <renatoosoouzaa26@gmail.com>
2023-05-08 11:04:52 -0300

    implementa exemplo de lista de oficinas da nossa casa

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import { ToDo } from './pages/todo/todo-page'
+import { WorkshopList } from './pages/workshop/index'
 
 export const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ToDo />} exact />
+        <Route path="/workshops" element={<WorkshopList/>} />
       </Routes>
     </BrowserRouter>
   )
