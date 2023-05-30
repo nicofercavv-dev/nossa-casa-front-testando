1e8d7031b09f2fc1797c4242ba8c7e484d1cefe6
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-05-30 13:13:46 -0300

    02/home-navber: finalizando o css com alterçoes de cores e evento e posicionamentos: -@angeloneves19 -@oGuilhermeSoares

--- src/components/navbar/styled.js
+++ src/components/navbar/styled.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 const NavBarComponent = styled.nav`
-  background-color: #F5BC4A;
+  background-color: black;
   height: 4em;
   display: flex;
   align-items: center;
@@ -15,7 +15,7 @@ const NavBarComponent = styled.nav`
    
     
     a {
-      color: black;
+      color: #ffffff;
       text-decoration: none;
       text-align: center;
       padding: 20px;
@@ -25,18 +25,26 @@ const NavBarComponent = styled.nav`
       font-size: 16px;
       line-height: 14px;
        /* identical to box height */
-
      text-align: center;
      text-transform: uppercase;
+
+     &:hover {
+     color: #F5BC4A;
+    }
     }
   }
 
+
   button {
-    background-color: #F2F5F7;
+    background: rgb(81,107,132);
+    background: linear-gradient(180deg, rgba(81,107,132,1) 0%, rgba(187,197,207,1) 50%, rgba(255,255,255,1) 100%);
     width: 146px;
     height: 40px;
-    border: 1px solid #424B5A;
     border-radius: 3px;
+    &:active {
+      color: background: rgb(81,107,132);
+      background: linear-gradient(360deg, rgba(81,107,132,1) 0%, rgba(187,197,207,1) 50%, rgba(255,255,255,1) 100%);
+    }
   }
 
   .container-logo {
@@ -44,8 +52,17 @@ const NavBarComponent = styled.nav`
     width: auto;
     height: 100%;
     display: flex; 
-  
+   
+
+  img{
+    width: 73px;
+    height: 67px;
+    left: 110px;
+    top: 67px;
+    border-radius: 250px;
   }
+  }
+
 
   @media (max-width: 768px) {
     /* Estilos para telas menores que 768px */
