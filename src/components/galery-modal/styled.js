f333facce239f7e562780c4cb96cc19b7e7708e7
oGuilhermeSoares <guilhermesoaresavilars@gmail.com>
2023-06-12 15:35:40 -0300

    05/home-modal-sobre: aplicando responsividade em algumas telas repassando código para minha dupla finalizar o resto da responsividade @oGuilhermeSoares @k-js

--- src/components/galery-modal/styled.js
+++ src/components/galery-modal/styled.js
@@ -1,13 +1,13 @@
 import styled from 'styled-components'
 
 export const ModalContainer = styled.div`
-  width: 100%;
+   width: 100%;  
   height: 100%;
   position: fixed;
   left: 0;
   top: 0;
   z-index: 1;
-  overflow: auto;
+  /* overflow: auto; */
   background-color: ${props => props.theme === 'about' ? '#F5BC4A' : '#516B84'};
 
   .exit {
@@ -37,7 +37,7 @@ export const ModalContainer = styled.div`
   .topic {
     height: 100%;
     width: 100%;
-    margin: 0 15px;
+    margin: 0 0px;;
 
     h3 {
       font-size: 1rem;
@@ -45,21 +45,113 @@ export const ModalContainer = styled.div`
     }
 
     p {
-      font-size: 0.93rem;
+      font-size: 1rem;
       text-align: justify;
     }
   }
 
-  @media (min-width: 320px) and (max-width: 700px) {
-      .content-container {
-          grid-template-columns: none !important;
-          grid-template-rows: none !important;
-        }
 
-        .about-title {
-          font-size: 1.8rem;
-          text-align: center;
-          margin-bottom: 60px;
-}
+  @media (min-width: 325px )    {
+    width: 53%;
+    overflow: auto;
+    .content-container {
+      height: 90%;
+      grid-template-columns: 1fr;
+      grid-template-rows: auto;
+      grid-row-gap: 20px;
+      grid-column-gap: 20px;
+    }
+
+    .about-title {
+      font-size: 1.9rem;
+      margin-bottom: 40px;
+    }
+
+    .exit {
+      width: 50px;
+      margin-left: 85%;
+    }
+
+    .topic {
+      margin: 0 0px;
+
+      h3 {
+        font-size: 1.4rem;
+        margin-bottom: 8px;
       }
+
+      p {
+        font-size: 1rem;
+      }
+    }
+  }
+
+  @media (min-width: 326px) and (max-width: 839px){
+    width: 53%;
+    overflow: auto;
+    .content-container {
+      height: 90%;
+      grid-template-columns: 1fr;
+      grid-template-rows: auto;
+      grid-row-gap: 20px;
+      grid-column-gap: 20px;
+    }
+
+    .about-title {
+      font-size: 1.9rem;
+      margin-bottom: 40px;
+    }
+
+    .exit {
+      width: 50px;
+      margin-left: 85%;
+    }
+
+    .topic {
+      margin: 0 0px;
+
+      h3 {
+        font-size: 1.4rem;
+        margin-bottom: 8px;
+      }
+
+      p {
+        font-size: 1rem;
+      }
+    }
+  }
+
+  @media (min-width: 840px) and (max-width: 1920px)  {
+    width: 100%;
+    overflow: auto;
+    .content-container {
+      height: 90%;
+      grid-template-columns: 1fr 1fr;
+      grid-template-rows: auto;
+      grid-row-gap: 30px;
+      grid-column-gap: 50px;
+    }
+
+    .about-title {
+      margin-bottom: 40px;
+    }
+
+    .exit {
+      width: 73px;
+      margin-left: 90%;
+    }
+
+    .topic {
+      margin: 0 0px;
+
+      h3 {
+        font-size: 1rem;
+        margin-bottom: 10px;
+      }
+
+      p {
+        font-size: 1rem;
+      }
+    }
+  }
 `
